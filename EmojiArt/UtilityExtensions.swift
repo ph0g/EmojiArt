8bd314496a8c3b149baa5f0834ec77de06f80e4d
Pierre-Hugues Oger <phylog@gmail.com>
2023-05-20 18:05:26 +0200

    Lecture 9: EmojiArt Drag and Drop Multithreading

---
+++ EmojiArt/UtilityExtensions.swift
@@ -0,0 +1,108 @@
+//
+//  UtilityExtensions.swift
+//  EmojiArt
+//
+//  Created by Pierre-Hugues Oger on 14/05/2023.
+//
+
+import SwiftUI
+
+// in a collection of identifiables
+// we often might want to find the element that has the same id
+// as an Identifiable we already have in hand
+// we name this index(matching:) instead of fisrtIndex(matching:)
+// because we assume that someone creating a Collection of Identifiable
+// is usually going to have only one of each Identifiable thing in there
+// (though there's nothing to restrict them from doing so; it's just a naming choice)
+
+extension Collection where Element: Identifiable {
+    func index(matching element: Element) -> Self.Index? {
+        firstIndex(where: { $0.id == element.id })
+    }
+}
+
+extension CGRect {
+    var center: CGPoint {
+        CGPoint(x: midX, y: midY)
+    }
+}
+
+extension CGPoint {
+    static func -(lhs: Self, rhs: Self) -> CGSize {
+        CGSize(width: lhs.x - rhs.x, height: lhs.y - rhs.y)
+    }
+    
+    static func +(lhs: Self, rhs: CGSize) -> CGPoint {
+        CGPoint(x: lhs.x + rhs.width, y: lhs.y + rhs.height)
+    }
+    
+    static func -(lhs: Self, rhs: CGSize) -> CGPoint {
+        CGPoint(x: lhs.x - rhs.width, y: lhs.y - rhs.height)
+    }
+    
+    static func *(lhs: Self, rhs: CGFloat) -> CGPoint {
+        CGPoint(x: lhs.x * rhs, y: lhs.y * rhs)
+    }
+    
+    static func /(lhs: Self, rhs: CGFloat) -> CGPoint {
+        CGPoint(x: lhs.x / rhs, y: lhs.y / rhs)
+    }
+}
+
+extension Array where Element == NSItemProvider {
+    func loadObjects<T>(ofType theType: T.Type, firstOnly: Bool = false, using load: @escaping (T) -> Void) -> Bool where T: NSItemProviderReading {
+        if let provider = first(where: { $0.canLoadObject(ofClass: theType) }) {
+            provider.loadObject(ofClass: theType) { object, error in
+                if let value = object as? T {
+                    DispatchQueue.main.async {
+                        load(value)
+                    }
+                }
+            }
+            return true
+        }
+        return false
+    }
+    
+    func loadObjects<T>(ofType theType: T.Type, firstOnly: Bool = false, using load: @escaping (T) -> Void) -> Bool where T: _ObjectiveCBridgeable, T._ObjectiveCType: NSItemProviderReading {
+        if let provider = first(where: { $0.canLoadObject(ofClass: theType) }) {
+            let _ = provider.loadObject(ofClass: theType) { object, error in
+                if let value = object {
+                    DispatchQueue.main.async {
+                        load(value)
+                    }
+                }
+            }
+            return true
+        }
+        return false
+    }
+    
+    func loadFirstObject<T>(ofType theType: T.Type, using load: @escaping (T) -> Void) -> Bool where T: NSItemProviderReading {
+        loadObjects(ofType: theType, using: load)
+    } 
+    
+    func loadFirstObject<T>(ofType theType: T.Type, using load: @escaping (T) -> Void) -> Bool where T: _ObjectiveCBridgeable, T._ObjectiveCType: NSItemProviderReading {
+        loadObjects(ofType: theType, firstOnly: true, using: load)
+    }
+}
+
+extension Character {
+    var isEmoji: Bool {
+        // Swift does not have a way to ask if a Character isEmoji
+        // but it does let us check to see if our component scalars isEmoji
+        // unfortunately unicode allows certain scalars (like 1)
+        // to be modified by another scalar to become emoji (e.g. 1️⃣)
+        // so the scalar "1" will report isEmoji = true
+        // so we can't just check to see if the first scalar isEmoji
+        // the quick and dirty here is to see if the scalar is at least the first true emoji we know of
+        // (the start of the "msicellaneous items" section)
+        // or check to see if this is a multiple scalar unicode sequence
+        // (e.g. a 1 with a unicode modifier to force it to be presented as emoji 1️⃣)
+        if let firstScalar = unicodeScalars.first, firstScalar.properties.isEmoji {
+            return (firstScalar.value >= 0x238d || unicodeScalars.count > 1)
+        } else {
+            return false
+        }
+    }
+}
